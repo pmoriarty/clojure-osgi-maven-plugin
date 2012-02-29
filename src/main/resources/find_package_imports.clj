3270d649afa1dbecb0c030ec326b7cdbe230cb46
Paudi Moriarty <pmoriarty@annadaletech.com>
2012-02-29 09:28:57 +0000

    Initial commit

---
+++ src/main/resources/find_package_imports.clj
@@ -0,0 +1,23 @@
+(ns user)
+
+(use 'clojure.pprint)
+(use 'clojure.java.io)
+(use 'com.annadaletech.clojure.osgi.utils)
+
+(defn check [dir manifest ignore-list]
+  (let [missing (find-missing-imports (file dir) (slurp manifest) (read-string (str "[" ignore-list "]")))]
+    (if (seq missing)
+      (do
+        (println "[ERROR]" manifest "is missing some imported packages: " \newline (with-out-str (pprint (sort missing))))
+        false)
+      true)))
+
+;(let [[dir manifest & ignore-list] *command-line-args*
+;      missing (find-missing-imports (file dir) (slurp manifest) ignore-list)]
+;  (if (seq missing)
+;    (do
+;      (println "[ERROR]" manifest "is missing some imported packages: " \newline (with-out-str (pprint (sort missing))))
+;      false)
+;    true))
+
+
